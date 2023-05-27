45a284a060700f2bb9d9ae17f4afc3280b410878
KUNAL <kunal_1201533@outlook.in>
2023-05-27 18:51:02 +0530

    Google-Authentication-added

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ require("dotenv").config();
 const session = require("express-session");
 const passport = require('passport');
 const passportLocalMongoose = require("passport-local-mongoose");
-
+const findOrCreate = require('mongoose-findorcreate')
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
 
 
 const app = express();
@@ -17,9 +18,9 @@ app.use(bodyParser.urlencoded({
 }))
 
 app.use(session({
-  secret: "Lpohhg fgfhn nbhnm",
-  resave: false,
-  saveUninitialized: false
+   secret: "Lpohhg fgfhn nbhnm",
+   resave: false,
+   saveUninitialized: false
 }))
 
 app.use(passport.initialize());
@@ -31,17 +32,41 @@ mongoose.connect("mongodb://127.0.0.1:27017/userDB", { useNewUrlParser: true });
 
 const userSchema = new mongoose.Schema({
    email: String,
-   password: String
+   password: String,
+   googleId: String
 });
 
 userSchema.plugin(passportLocalMongoose)
+userSchema.plugin(findOrCreate)
 
 const User = mongoose.model("User", userSchema);
 
 passport.use(User.createStrategy());
 
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+passport.serializeUser(function(user, done) {
+   done(null, user.id);
+ });
+ 
+ passport.deserializeUser(function(id, done) {
+   User.findById(id, function (err, user) {
+     done(err, user);
+   });
+ });
+
+passport.use(new GoogleStrategy({
+   clientID: process.env.CLIENT_ID,
+   clientSecret: process.env.CLIENT_SECRET,
+   callbackURL: "http://localhost:3000/auth/google/secrets",
+   // userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
+},
+   
+   function (accessToken, refreshToken, profile, cb) {
+      // console.log(profile)
+      User.findOrCreate({ googleId: profile.id }, function (err, user) {
+         return cb(err, user);
+      });
+   }
+));
 
 
 
@@ -61,49 +86,62 @@ app.get("/register", function (req, res) {
 
 })
 
-app.get("/logout",function(req,res){
-   req.logout(err=>console.log(err));
-   res.redirect("/");
+app.get('/logout', function(req, res, next){
+   req.logout(function(err) {
+     if (err) { return next(err); }
+     res.redirect('/');
+   });
+ });
+
+
+app.get('/auth/google',
+   passport.authenticate('google', { scope: ['profile'] }));
+
+
+app.get('/auth/google/secrets', 
+passport.authenticate('google', { failureRedirect: '/login' }),
+function(req, res) {
+   // Successful authentication, redirect home.
+   res.redirect('/secrets');
 });
 
 
-
-app.get("/secrets",function(req,res){
-   if(req.isAuthenticated()){
+app.get("/secrets", function (req, res) {
+   if (req.isAuthenticated()) {
       res.render("secrets");
-      
-   }else{
+
+   } else {
       res.redirect("/login");
    }
 })
 
 app.post("/register", function (req, res) {
-   User.register({username:req.body.username},req.body.password,function(err,user){
-       if(err){
+   User.register({ username: req.body.username }, req.body.password, function (err, user) {
+      if (err) {
          console.log(err);
          res.redirect("/register");
-       }
-       else{
-         passport.authenticate("local")(req,res,function(){
+      }
+      else {
+         passport.authenticate("local")(req, res, function () {
             res.redirect("/secrets")
          })
-       }
+      }
    })
 })
 
 
 app.post("/login", function (req, res) {
-   const user= new User({
-      username : req.body.username,
-      password : req.body.password
+   const user = new User({
+      username: req.body.username,
+      password: req.body.password
    });
-   
-   req.login(user,function(err){
-      if(err){
-         console.log(err);
+
+   req.login(user, function (err) {
+      if (err) {
+         console.log(user);
          res.redirect("/login")
-      }else{
-         passport.authenticate("local")(req,res,function(){
+      } else {
+         passport.authenticate("local")(req, res, function () {
             res.redirect("/secrets");
          })
       }
